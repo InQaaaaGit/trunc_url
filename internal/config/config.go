aef4e5e0ea660393b53990ad13702b9410c3faf4
goryshkin <goryshkin@cdo-global.ru>
2025-04-12 14:08:40 +0300

    iter4 start from cdm

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,11 +1,26 @@
 package config
 
+import "flag"
+
+// Config хранит конфигурацию приложения.
 type Config struct {
-	ServerAddress string
+	ServerAddress string // Адрес для запуска HTTP-сервера (-a)
+	BaseURL       string // Базовый адрес для сокращенных URL (-b)
 }
 
+// NewConfig инициализирует конфигурацию, парсит флаги командной строки.
 func NewConfig() *Config {
-	return &Config{
-		ServerAddress: ":8080",
-	}
+	cfg := &Config{}
+
+	// Определение флагов командной строки
+	// Первый аргумент - имя флага
+	// Второй - значение по умолчанию
+	// Третий - описание флага
+	flag.StringVar(&cfg.ServerAddress, "a", ":8080", "Адрес запуска HTTP-сервера (формат: хост:порт)")
+	flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080", "Базовый адрес результирующего сокращённого URL")
+
+	// Парсинг флагов
+	flag.Parse()
+
+	return cfg
 }
