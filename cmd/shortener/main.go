5347ebe70ca0893204bfb2941bad2689cdffeef9
goryshkin <goryshkin@cdo-global.ru>
2025-08-09 20:01:02 +0300

    try fix16 iter

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	_ "net/http/pprof"
 
 	"github.com/InQaaaaGit/trunc_url.git/internal/app"
-	"github.com/InQaaaaGit/trunc_url.git/internal/config"
+	"github.com/InQaaaaGit/trunc_url.git/internal/buildinfo"
+	"github.com/InQaaaaGit/trunc_url.git/internal/server"
 	"go.uber.org/zap"
 )
 
@@ -17,33 +16,17 @@ var (
 	buildCommit  = "N/A"
 )
 
-// printBuildInfo выводит информацию о сборке приложения
-func printBuildInfo() {
-	fmt.Printf("Build version: %s\n", buildVersion)
-	fmt.Printf("Build date: %s\n", buildDate)
-	fmt.Printf("Build commit: %s\n", buildCommit)
-}
-
 func main() {
-	// Выводим информацию о сборке
-	printBuildInfo()
+	// Создаем и выводим информацию о сборке
+	buildInfo := buildinfo.NewInfo(buildVersion, buildDate, buildCommit)
+	buildInfo.Print()
 
 	// Инициализация логгера
-	logger, err := zap.NewProduction()
-	if err != nil {
-		log.Fatalf("Ошибка инициализации логгера: %v", err)
-	}
-	defer func() {
-		if err := logger.Sync(); err != nil {
-			log.Printf("Ошибка синхронизации логгера: %v", err)
-		}
-	}()
+	logger, cleanup := server.InitLogger()
+	defer cleanup()
 
 	// Инициализация конфигурации
-	cfg, err := config.NewConfig()
-	if err != nil {
-		logger.Fatal("Ошибка инициализации конфигурации", zap.Error(err))
-	}
+	cfg := server.InitConfig(logger)
 
 	// Создание и настройка приложения
 	application, err := app.NewApp(cfg)
@@ -54,21 +37,9 @@ func main() {
 		logger.Fatal("Ошибка конфигурации приложения", zap.Error(err))
 	}
 
-	// Запуск сервера
-	if cfg.IsHTTPSEnabled() {
-		server := application.GetServer()
-		logger.Info("Запуск HTTPS сервера",
-			zap.String("address", cfg.ServerAddress),
-			zap.String("cert", cfg.TLSCertFile),
-			zap.String("key", cfg.TLSKeyFile))
-		if err := server.ListenAndServeTLS(cfg.TLSCertFile, cfg.TLSKeyFile); err != nil {
-			logger.Fatal("HTTPS server failed to start", zap.Error(err))
-		}
-	} else {
-		server := application.GetServer()
-		logger.Info("Запуск HTTP сервера", zap.String("address", cfg.ServerAddress))
-		if err := server.ListenAndServe(); err != nil {
-			logger.Fatal("HTTP server failed to start", zap.Error(err))
-		}
+	// Создание и запуск сервера
+	httpServer := server.NewHTTPServer(application.GetServer(), cfg, logger)
+	if err := httpServer.Start(); err != nil {
+		logger.Fatal("Server failed to start", zap.Error(err))
 	}
 }
